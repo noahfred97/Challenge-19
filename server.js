33dd771c8cdb204c2882b9bd86a628726152929c
noahfred97 <90789790+noahfred97@users.noreply.github.com>
2022-03-08 15:11:09 -0500

    2nd

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, "mongodb://localhost:27017/budget-tracker", {
+mongoose.connect(MONGODB_URI, "mongodb://localhost:27017/BudgetTracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
